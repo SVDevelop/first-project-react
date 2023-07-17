2c2cca868f742527e293869c749b0fac13c0fa32
Maxim Safronov <safronovmaxiim@mail.ru>
2023-07-17 19:57:38 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => { 
+    return ( <header className="header">
+        <img src="https://avatars.mds.yandex.net/i?id=0663c7fca5ed75c97d0100964cf4179131ae649d-7593439-images-thumbs&n=13" alt = "PlayStation Logo"/>
+    </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+      <div className="content">
+      <div>
+      <img src ="https://avatars.mds.yandex.net/i?id=2eeb65844afee5dc830414a1fc1e54c152628f4f-9242682-images-thumbs&n=13"></img>
+      </div>
+      <div>ava + description</div>
+      <div>my post
+        <div>new post</div>
+        <div>
+          New post
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default Profile;
