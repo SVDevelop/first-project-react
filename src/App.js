9f361cd58cc3f5ec8ad759a3a3c5d50c88fd4151
Maxim Safronov <safronovmaxiim@mail.ru>
2023-07-21 22:28:13 +0300

    Adding NavLink to NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React, { Profiler } from "react";
 import './App.css';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import Music from './components/Music/Music';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <NavBar />
-      <div className='app-wrapper-content'>
-        <BrowserRouter>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <NavBar />
+        <div className='app-wrapper-content'>
           <Routes>
             <Route path='/profile' element={<Profile />} />
             <Route path='/dialogs' element={<Dialogs />} />
@@ -24,9 +24,9 @@ const App = () => {
             <Route path='/music' element={<Music />} />
             <Route path='/settings' element={<Settings />} />
           </Routes>
-        </BrowserRouter>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
